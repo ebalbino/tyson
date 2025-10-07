005e544e2235a78ae35fd1ea7ccdaefefa67e12d
Ed Balbino <ed.balbino@zigurate.dev>
2025-10-07 17:11:09 -0300

    Reimplement to use arena allocator.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tyson::{Arena, MemoryBlock, make, strmake};
+use tyson::{Arena, Array, List, MemoryBlock, make, strmake};
 
 mod parser;
 
@@ -12,18 +12,18 @@ const CODE: &str = "(print 1 3.14159 \"hello, world\")\n (+ (* 2 2) (/ 10 5)";
 const JSON: &str = "{ \"type\": \"object\" }";
 const RUBY_OBJ: &str = "{ \"type\"=> \"object\" }";
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Instance<'arena> {
     Integer(i64),
     Double(f64),
     String(&'arena str),
     Symbol(&'arena str),
-    List(Vec<Instance<'arena>>),
+    List(Array<Instance<'arena>>),
 }
 
 fn parse_list<'arena>(
     arena: &'arena Arena,
-    tokens: &mut Vec<Token>,
+    tokens: &mut Array<Token>,
 ) -> Result<Instance<'arena>, &'static str> {
     let token = tokens.pop();
 
@@ -34,27 +34,34 @@ fn parse_list<'arena>(
         return Err("The first token must be an opening parentheses");
     }
 
-    let mut list: Vec<Instance<'arena>> = Vec::new();
+    let mut list = make!(arena, Instance, 8)
+        .map(Array::new)
+        .expect("Unable to allocate list.");
+
     while !tokens.is_empty() {
         match tokens.pop() {
             None => {
                 return Err("Not enough tokens");
             }
             Some(token) => match token {
-                Token::Integer(i) => list.push(Instance::Integer(i)),
-                Token::Float(f) => list.push(Instance::Double(f)),
+                Token::Integer(i) => {
+                    list.push(&Instance::Integer(*i));
+                }
+                Token::Float(f) => {
+                    list.push(&Instance::Double(*f));
+                }
                 Token::String(s) => {
                     let s = strmake!(arena, "{}", s).expect("No memory to allocate string");
-                    list.push(Instance::String(s));
+                    list.push(&Instance::String(s));
                 }
                 Token::Symbol(s) => {
                     let s = strmake!(arena, "{}", s).expect("No memory to allocate symbol");
-                    list.push(Instance::Symbol(s));
+                    list.push(&Instance::Symbol(s));
                 }
                 Token::LParen | Token::LBrace | Token::LBracket => {
-                    tokens.push(Token::LParen);
+                    tokens.push(&Token::LParen);
                     let sub_list = parse_list(arena, tokens)?;
-                    list.push(sub_list);
+                    list.push(&sub_list);
                 }
                 Token::RParen | Token::RBrace | Token::RBracket => {
                     return Ok(Instance::List(list));
@@ -71,16 +78,24 @@ fn main() {
     let arena = block.arena(megabytes(16)).unwrap();
 
     for text in &[CODE, JSON, RUBY_OBJ] {
-        let mut tokens = Tokenizer::new(text).into_iter().collect::<Vec<_>>();
-        tokens.reverse();
+        let token_count = Tokenizer::new(text).into_iter().count();
+        let mut tokens = make!(arena, Token, token_count)
+            .map(Array::new)
+            .expect("Not enough space for tokens");
 
-        let mut statements = Vec::new();
-
-        while !tokens.is_empty() {
-            statements.push(parse_list(&arena, &mut tokens).unwrap());
+        for token in Tokenizer::new(text) {
+            tokens.push(&token);
         }
 
-        for stmt in &statements {
+        tokens.reverse();
+
+        let mut statements = List::new(&arena);
+
+        while !tokens.is_empty() {
+            statements.push_back(&parse_list(&arena, &mut tokens));
+        }
+
+        for stmt in statements.iter() {
             println!("{:?}", stmt);
         }
     }
